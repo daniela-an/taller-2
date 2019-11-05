a79fc657ca8add173b53df1a1e2bf5a5e8623323
estudiante <estudiante@SC302e23.local>
2019-11-05 12:40:22 -0500

    taller terminado

--- index.js
+++ index.js
@@ -14,13 +14,15 @@ app.use(express.json());
 const MongoClient = require('mongodb').MongoClient;
 const url = 'mongodb://localhost:27017';
 const dbName = 'store';
-const client = new MongoClient(url, { useUnifiedTopology: true });
+const client = new MongoClient(url, {
+    useUnifiedTopology: true
+});
 var db = null;
- 
-client.connect(function(err){
-    if(err){
-      console.error(err);
-      return;
+
+client.connect(function (err) {
+    if (err) {
+        console.error(err);
+        return;
     }
     db = client.db(dbName);
 });
@@ -32,58 +34,73 @@ app.engine('handlebars', hbs({
     layoutsDir: path.join(__dirname, 'views')
 }));
 
-app.set('view engine','handlebars');
+app.set('view engine', 'handlebars');
 
 app.get('/', function (request, response) {
     response.render('main');
 });
 
-app.get('/store', function(request,response){
+app.get('/store', function (request, response) {
     const coleccion = db.collection('productos');
-    var obj = {}, va = request.query.var;
-    if(va !== 'general' && va !== 'ordenar')obj[va] = { '$eq' : request.query.val };
-    
-    coleccion.find(obj).toArray(function(err,docs){
-        if(err){
+    var obj = {},
+        va = request.query.var;
+    if (va !== 'general' && va !== 'ordenar') obj[va] = {
+        '$eq': request.query.val
+    };
+
+    coleccion.find(obj).toArray(function (err, docs) {
+        if (err) {
             console.log(err);
             response.send(err);
             return;
         }
-        if(va === 'ordenar') docs.sort((a, b) => {return (a.precio - b.precio)});
+        if (va === 'ordenar') docs.sort((a, b) => {
+            return (a.precio - b.precio)
+        });
         var contexto = {
-            productos: docs 
+            productos: docs
         };
         response.render('aretes', contexto);
     });
 });
 
-app.get('/detalle', function(request, response){
+app.get('/detalle', function (request, response) {
     const coleccion = db.collection('productos');
     var prod = request.query.producto;
 
-    coleccion.find( { nombre: { '$eq' : prod} } ).toArray(function(err,docs){
-        if(err){
+    coleccion.find({
+        nombre: {
+            '$eq': prod
+        }
+    }).toArray(function (err, docs) {
+        if (err) {
             console.log(err);
             response.send(err);
             return;
         }
-        response.render('detalle', { producto: docs, nombre: docs[0].nombre});
+        response.render('detalle', {
+            producto: docs,
+            nombre: docs[0].nombre
+        });
     });
 });
 
-app.get('/checkout', function(request, response){
+app.get('/checkout', function (request, response) {
     const coleccion = db.collection('carrito');
-    coleccion.find({}).toArray(function(err, docs){
-        if(err){
+    coleccion.find({}).toArray(function (err, docs) {
+        if (err) {
             console.log(err);
             response.send(err);
             return;
         }
         var total = 0;
-        docs.map((elem)=>{
+        docs.map((elem) => {
             total += elem.precio;
         });
-        response.render('checkout', {productos: docs, total: total });
+        response.render('checkout', {
+            productos: docs,
+            total: total
+        });
     });
 });
 
@@ -91,42 +108,56 @@ app.get('/informacion', function (request, response) {
     response.render('informacion');
 });
 
-app.post('/api/AgregarAlCarrito', function(request, response){
+app.post('/api/AgregarAlCarrito', function (request, response) {
     const coleccion = db.collection('productos');
     const coleccion2 = db.collection('carrito');
     let titulo = request.body.nombre;
     let cant = request.body.cantidad;
 
     coleccion.find({
-        nombre:{
-            '$eq' : titulo
-        }
-    })
-    .toArray(function(err, doc){ 
-        if(err){
-            console.log(err);
-            response.send(err);
-            return;
-        } 
-        for (let i = 0; i < parseInt(cant); i++) {
-            console.log("insertò"+doc);
-            coleccion2.insert({
-                categoria : doc[0].categoria,
-                estilo : doc[0].estilo,
-                descripción : doc[0].descripción,
-                nombre : doc[0].nombre,
-                precio : doc[0].precio,
-                color : doc[0].color,
-                imagenes : doc[0].imagenes
-            });
-        }
-    });
+            nombre: {
+                '$eq': titulo
+            }
+        })
+        .toArray(function (err, doc) {
+            if (err) {
+                console.log(err);
+                response.send(err);
+                return;
+            }
+            for (let i = 0; i < parseInt(cant); i++) {
+                console.log("insertò" + doc);
+                coleccion2.insert({
+                    categoria: doc[0].categoria,
+                    estilo: doc[0].estilo,
+                    descripción: doc[0].descripción,
+                    nombre: doc[0].nombre,
+                    precio: doc[0].precio,
+                    color: doc[0].color,
+                    imagenes: doc[0].imagenes
+                });
+            }
+        });
 });
 
-app.post('/api/vaciarCarrito', function(request, response){
+app.post('/api/vaciarCarrito', function (request, response) {
     const coleccion = db.collection('carrito');
     coleccion.remove({});
     response.send("borrado");
+});
+
+app.post('/api/nuevaSolicitud', function (request, response) {
+    const coleccion = db.collection('peticiones');
+    coleccion.insert({
+        nombre: request.body.nombre,
+        apellido: request.body.nombre,
+        cedula: request.body.cedula,
+        direccion: request.body.direccion,
+        cuenta: request.body.cuenta,
+        fecha: request.body.fecha,
+        codigo: request.body.codigo
+    });
+    response.send("Nueva solicitud creada");
 });
 
 Handlebars.registerPartial('nav', `<div class="navBar"><div class="logo"><img class="styleImgLogo" src="./image/imagenes/Logo.png" alt="logo Emilia"></div><div class="navText"><ul class="listText"><li class="barItems"><a class="navBarItems" href="/store?var=general&val=general">Productos</a><ul><li><a href="/store?var=general&val=general">Aretes</a></li><li><a href="">Pulseras</a></li><li><a href="">Collares</a></li><li><a href="">Tobilleras</a></li></ul></li><li class="barItems"><a class="navBarItems" href="#">Tienda</a></li><li class="barItems"><a class="navBarItems" href="#">Contactános</a></li></ul></div><div class="navImg"><ul class="listImg"><li class="barImg"><img class="styleImgMenu" src="./image/imagenes/search.svg" alt=""></li><li class="barImg"><img class="styleImgMenu" src="./image/imagenes/user.svg" alt=""></li><li class="barImg"><img class="styleImgMenu" src="./image/imagenes/shopping-bag.svg" alt=""></li></ul></div></div>`);
